4eebfa2c181da383304e2b11e8277f6cae054778
MegiBegi <ms.magdalena.kozlowska@gmail.com>
2020-02-08 21:13:36 +0100

    eslint fixed - prettier installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import React, { FC, useCallback, useMemo, useReducer, useRef, useContext, useLayoutEffect } from 'react'
+import React, { FC } from 'react'
 
-const App: FC = () => {
-  const array = [4, 3, 5, 6, 7, 7, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 15, 16, 18]
-  console.log(array)
-  const funcs = () => { return (<h1>"aaa"</h1>) }
-  return (<div />)
-}
+const App: FC = () => <div />
 export default App
