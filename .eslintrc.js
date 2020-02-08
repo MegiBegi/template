7792d8aa53647a2945b741d5969c9e7990a35195
MegiBegi <ms.magdalena.kozlowska@gmail.com>
2020-02-08 19:13:29 +0100

    reinstalling all packages

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['airbnb-typescript'],
-  rules: {
-    semi: ["error", "never"],
-    "@typescript-eslint/semi": ["error", "never"],
-  }
-};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback, useMemo, useReducer, useRef, useContext, useLayoutEffect } from 'react'
 
-const App: FC = () => <div />
-
+const App: FC = () => {
+  const array = [4, 3, 5, 6, 7, 7, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 14]
+  console.log(array)
+  const funcs = () => { return (<h1></h1>) }
+  return (<div />)
+}
 export default App
