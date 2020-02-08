baca1eddfdb4021aaf9bf668d18379ed284673de
MegiBegi <ms.magdalena.kozlowska@gmail.com>
2020-02-08 19:35:11 +0100

    deleting yarn.lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useCallback, useMemo, useReducer, useRef, useContext, useLayoutEffect } from 'react'
 
 const App: FC = () => {
-  const array = [4, 3, 5, 6, 7, 7, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 14]
+  const array = [4, 3, 5, 6, 7, 7, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 15, 16, 18]
   console.log(array)
-  const funcs = () => { return (<h1></h1>) }
+  const funcs = () => { return (<h1>"aaa"</h1>) }
   return (<div />)
 }
 export default App
